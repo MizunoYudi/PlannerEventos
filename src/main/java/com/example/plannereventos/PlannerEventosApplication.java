003e171e4c4e1ddb5a0923ccdabb45baac127dff
Helicassia <helicassiajesus44@gmail.com>
2026-08-18 20:12:46 -0300

    "Adicionando o Projeto"

---
+++ src/main/java/com/example/plannereventos/PlannerEventosApplication.java
@@ -0,0 +1,13 @@
+package com.example.plannereventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerEventosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerEventosApplication.class, args);
+    }
+
+}
